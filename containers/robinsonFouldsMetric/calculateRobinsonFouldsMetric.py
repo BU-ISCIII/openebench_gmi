cc4763ea954038f9afb858f74655039d1e2daf56
saramonzon <sara.monzon.fdz@gmail.com>
2019-05-07 14:43:01 +0200

    Almost working generate allvsall robinson-foulds

--- containers/robinsonFouldsMetric/calculateRobinsonFouldsMetric.py
+++ containers/robinsonFouldsMetric/calculateRobinsonFouldsMetric.py
@@ -6,6 +6,7 @@ import argparse
 import json
 import subprocess
 from ete3 import Tree
+import shutil
 
 #################
 ### FUNCTIONS ###
@@ -31,7 +32,7 @@ def check_arg (args=None) :
 
     parser.add_argument('--tree_file1','-t1', required= True, help ='Path to tree file 1')
     parser.add_argument('--benchmark_trees_path','-b', required= True, help ='Path to benchmark path where other participants tree are found.')
-    parser.add_argument('--output' ,'-o',required= False, help = 'Path to result metric json.Default = robinfoulds.json', default="robinfoulds.json")
+    parser.add_argument('--output' ,'-o',required= False, help = 'Path to result metric json.Default = participant_matrix.json', default="participant_matrix.json")
     parser.add_argument('--event_id','-e' ,required= False, help = 'OpenEbench event identifier', default="default")
     parser.add_argument('--participant_id','-p' ,required= False, help = 'OpenEbench participant identifier', default="default")
 
@@ -41,11 +42,15 @@ def check_arg (args=None) :
 #################
 ### FUNCTIONS ###
 #################
- def midpoint_root (tree):
+def midpoint_root (tree):
     tree_outgroup = tree.get_midpoint_outgroup()
     tree.set_outgroup(tree_outgroup)
     return tree
-
+def collapse_nodes (tree, branch_length):
+    for node_tree in tree.get_descendants():
+        if not node_tree.is_leaf() and node_tree.dist <= branch_length:
+            node_tree.delete()
+    return tree
 
 ###################
 ### MAIN SCRIPT ###
@@ -60,6 +65,9 @@ if __name__ == '__main__' :
     metrics = {}                          # IDs dictionary
     metrics_info1 = {}                     # Leaves list
     metrics_info2 = {}
+    participants = []
+    participant_row = []
+    tree_files = []
 
     # Grab arguments
     arguments = check_arg(sys.argv[1:])
@@ -73,64 +81,73 @@ if __name__ == '__main__' :
         print("Benchmark path with public participants trees not found.")
         sys.exit(1)
 
+    # Read benchmark data
+    in_file =  os.path.join(arguments.benchmark_trees_path,"participant_matrix.json")
+
+    with open(in_file) as json_file:
+        data = json.load(json_file)
+
+    participants = data["participants"]
+    participants.append(arguments.participant_id)
+    benchmark_data = data["matrix"]["values"]
+
+    try:
+        shutil.copy(arguments.tree_file1,arguments.benchmark_trees_path)
+    except:
+        print("New participant tree couldn't be copied to benchmark folder.")
 
     # Read file, check it is in the correct format.
     try:
-        print("Reading participant tree...")
-        tree1 = Tree(arguments.tree_file1)
-        for public_participant in os.listdir(benchmark_trees_path):
-            part_fullpath = os.path.join(benchmark_trees_path,public_participant)
-            if fnmatch.fnmatch(public_participant,"*.nwk") and os.path.isfile(part_fullpath):
-                participants.append(public_participant)
-        print ("Reading public participants trees...")
-        print("Setting root on midpoint...")
+        print("Reading participant tree, midpoint rooting and collapsing nodes...")
+        tree_test = Tree(arguments.tree_file1)
+        tree_test = midpoint_root(tree_test)
+        tree_test = collapse_nodes(tree_test,1.00000050002909e-06)
 
-        print("Collapsing nodes with branch distance = 0...")
-        for node_tree1 in tree1.get_descendants():
-            #print(node2.dist)
-            #if not node2.is_leaf() and round(node2.dist,4) <= 1:
-            if not node_tree1.is_leaf() and node_tree1.dist <= 1.00000050002909e-06:
-                node_tree1.delete()
-        for node_tree2 in tree2.get_descendants():
-            #print(node2.dist)
-            #if not node2.is_leaf() and round(node2.dist,4) <= 1:
-            if not node_tree2.is_leaf() and node_tree2.dist <= 1.00000050002909e-06:
-                node_tree2.delete()
-        print("Trees read successfully.")
     except:
-        print("Trees couldn't be loaded.")
+        print("Failed to read and analyzed test tree.")
         raise
         sys.exit(1)
 
-    # Calculate Robinson-Foulds distance between two trees REF and test
     try:
-        print("Calculating robinson-foulds distance...")
-        results = tree1.compare(tree2)
-        print("Calculation of robin-foulds distance failed.")
+        print ("Reading public participants trees...")
+        for public_participant in participants:
+            tree_file = public_participant + ".nwk"
+            part_fullpath = os.path.join(arguments.benchmark_trees_path,tree_file)
+            if os.path.isfile(part_fullpath):
+                tree_files.append(part_fullpath)
+
+        for participant in tree_files:
+            print("Reading public participant tree :" + participant)
+            tree = Tree(participant)
+            print("Setting root on midpoint...")
+            tree = midpoint_root (tree)
+            print("Collapsing nodes with branch distance = 0...")
+            tree = collapse_nodes (tree, 1.00000050002909e-06 )
+            results = tree_test.compare(tree)
+            participant_row.append(results["norm_rf"])
+
+        print("Public participants trees read and analyzed successfully.")
     except:
-        print("Robinson-foulds distance calculation failed.")
+        print("Public participants trees couldn't be analyzed.")
         raise
         sys.exit(1)
-
 
     #Create ids dictionary with event_id and sample ids from tree leaves.
     try:
-        metrics.update(event_id = arguments.event_id)
-        metrics.update(participant_id = arguments.participant_id)
-        metrics_info1.update(type="metrics",units="none",name="Robinson-Foulds metric",value=results["rf"])
-        metrics_info2.update(type="metrics",units="none",name="Normalized Robinson-Foulds metric",value=results["norm_rf"])
-        metrics.update(metrics = {'x' : metrics_info1, 'y' : metrics_info2 })
-
-        print("Successfully extracted ids from tree file.")
+        print("Updating benchmark data with new participant..")
+        benchmark_data.append(participant_row)
+        data.update(participants=participants)
+        data.update(matrix = {"values" : benchmark_data } )
     except:
-        print("Conversion/printing to newick failed.")
         raise
         sys.exit(1)
 
     # Outputting in json format
     try:
-        with open (arguments.output,"w") as write_file:
-            json.dump(metrics,write_file,indent=4)
+        path_output = os.path.join(arguments.benchmark_trees_path,arguments.output)
+        print("Creating json output: " + path_output)
+        with open (path_output,"w") as write_file:
+            json.dump(data,write_file,indent=4)
         write_file.close()
         print("Successfully created json output with ids.")
     except:
@@ -138,5 +155,3 @@ if __name__ == '__main__' :
         raise
         sys.exit(1)
 
-    if os.path.isfile(tree_file1):
-        shutil.copy(tree_file1,benchmark_trees_path)
